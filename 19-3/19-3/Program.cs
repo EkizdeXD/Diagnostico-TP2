28076f866b0ef5d26df45332ef30199a144a7d78
Ekizde <alexisamena4@gmail.com>
2025-03-19 22:47:10 -0300

    carga de diagnostico 2

---
+++ 19-3/19-3/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _19_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int opcion;
+            Console.WriteLine("Seleccione un numero del 1 al 5");
+            opcion = int.Parse(Console.ReadLine());
+            switch (opcion)
+            {
+                case 1:
+                    int n;
+                    int n2;
+                    int n3;
+                    int n4;
+                    int n5;
+                    Console.WriteLine("Ingrese 5 nombres de canciones");
+                    Console.WriteLine("Ingrese nombre de la cancion nro 1");
+                    n = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Ingrese nombre de la cancion nro 2");
+                    n2 = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Ingrese nombre de la cancion nro 3");
+                    n3 = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Ingrese nombre de la cancion nro 4");
+                    n4 = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Ingrese nombre de la cancion nro 5");
+                    n5 = int.Parse(Console.ReadLine());
+                    while (true)
+                    {
+                        string[] vector = new string[4];
+                    }
+                    break;
+            }
+        }
+    }
+}
